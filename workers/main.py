48706ed04c11f1e50fcaa999a1c62bed9577c853
kenstars <kannanpiedy@gmail.com>
2020-04-23 22:40:28 +0530

    Modified codes to fix bugs and to get a valid response from csv.

--- workers/main.py
+++ workers/main.py
@@ -1,12 +1,34 @@
 import os
 import json
 from flask import Flask, request
-
 import aiml
 import pandas as pd
+from nltk.corpus import wordnet 
+from nltk.stem import PorterStemmer
 
 from collections import defaultdict
 from workers.modules import get_value_dict, send_response, send_to_ui, get_generic_response
+from nltk.util import ngrams
+
+stemmer= PorterStemmer() 
+stem = lambda x: stemmer.stem(x)
+
+def get_syns(x):
+   synonyms = []
+   for syn in wordnet.synsets(x): 
+      for l in syn.lemmas(): 
+         synonyms.append(l.name().lower())
+   return list(set(synonyms))
+
+def all_ngram_merger(list_val):
+   return sum([[" ".join(j) for j in list(ngrams(list_val,i)) ]for i in range(1,len(list_val))], [])
+
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
 
 class ChatAnswer:
    def __init__(self):
@@ -17,9 +39,23 @@ class ChatAnswer:
       self.input_df = pd.read_csv("data/MasterData.csv")
       self.columnNames = self.input_df.columns
       self.value_dict = defaultdict(list)
+      self.synonym_dict = {}
       for eachColumnName in self.columnNames:
+         print(self.input_df[eachColumnName].tolist()[0])
          self.value_dict[eachColumnName] = get_value_dict(self.input_df[eachColumnName])
-      self.core_words = set(sum(self.value_dict.values(), [])+ list(self.columnNames))
+         self.synonym_dict[eachColumnName] = get_syns(eachColumnName)
+         if "name" in eachColumnName.lower():
+            self.synonym_dict[eachColumnName].append("who")
+         if is_number(str(self.input_df[eachColumnName].tolist()[0])):
+            self.synonym_dict[eachColumnName].append("how much")
+
+      print (self.synonym_dict)
+      self.core_words = set( sum([ 
+                                    str(i).lower().split() for i in sum(self.value_dict.values(), []) 
+                                  ]
+                                 ,[]) 
+                                 + [c.lower() for c in self.columnNames ] 
+                                 + sum(self.synonym_dict.values(), []))
       self.active_filters = []
       self.clarification_question_ask = 0
       self.genericResponseKernel = aiml.Kernel()
@@ -31,7 +67,6 @@ class ChatAnswer:
          path = "additional_resources/botdata/"+each_folder
          filenames = os.listdir(path)
          for eachFilename in filenames:
-            print ("Learning from ", path+"/"+eachFilename)
             self.genericResponseKernel.learn(path+"/"+eachFilename)
 
       print("kernel Ready")
@@ -50,27 +85,37 @@ def chat_worker():
    """
    message = request.form.get("message")
    json_parsed_data = {"message":message}
-   chat_grams = json_parsed_data["message"].split()
-   is_in_topic = chatAnswerObj.core_words.intersection(chat_grams)
+   unigrams = json_parsed_data["message"].lower().split()
+   stem_chat_grams = all_ngram_merger([stem(i) for i in unigrams])
+   chat_grams = all_ngram_merger(unigrams)
+   is_in_topic = chatAnswerObj.core_words.intersection(unigrams)
    if chatAnswerObj.clarification_question_ask:
       selected_df = chatAnswerObj.saved_df
       active_filters = chatAnswerObj.active_filters
       chatAnswerObj.clarification_question_ask = 0
    else:
+      active_filters = []
       selected_df =chatAnswerObj.input_df
    if is_in_topic:
+      print(is_in_topic)
       column_found = []
       values_matched_column = []
       for each_columnName in chatAnswerObj.columnNames:
-         if each_columnName in chat_grams:
+         if each_columnName.lower() in chat_grams:
             column_found.append(each_columnName)
+         else:
+            for eachSyn in chatAnswerObj.synonym_dict[each_columnName]:
+               if stem(eachSyn) in stem_chat_grams or eachSyn in chat_grams:
+                  column_found.append(each_columnName)
+                  break
          matched_sets = set(chatAnswerObj.value_dict[each_columnName]).intersection(chat_grams)
          if matched_sets:
             values_matched_column.append(each_columnName)
       select_column = values_matched_column + column_found
+      select_column = list(set(select_column))
       if values_matched_column:
          for each_column in values_matched_column:
-            selected_df["message_result"] = chatAnswerObj.input_df[each_column].apply(lambda x: int(x in json_parsed_data["message"]) or [0, 2][bool(set(x).intersection(chat_grams))] )
+            selected_df["message_result"] = chatAnswerObj.input_df[each_column].apply(lambda x: int(str(x).lower() in json_parsed_data["message"].lower()) or [0, 2][bool(set(str(x).lower().split()).intersection(unigrams))] )
             selected_values_df = selected_df[selected_df["message_result"] == 1]
             if selected_values_df.shape[-1] > 0:
                selected_df = selected_values_df
--- workers/modules.py
+++ workers/modules.py
@@ -1,8 +1,10 @@
+import json
+
 def get_value_dict(column_values):
     """
     takes a pandasSeriesInput of column values
     """
-    return column_values.tolist()
+    return [str(i).lower() for i in column_values.tolist()]
 
 def send_to_ui(sending_message):
     print("Message is :", sending_message)
@@ -11,9 +13,14 @@ def send_to_ui(sending_message):
 
 def send_response(selected_df, active_filters, select_column, question_to_be_asked):
     if question_to_be_asked:
-       response_msg = "which of the following " +active_filters[0]+ " are you looking for :  "+ ", ".join(active_filters[1])       
+        response_msg = "which of the following " +active_filters[0]+ " are you looking for :  "+ ", ".join(active_filters[1])       
     else:
-       response_msg = "The requested answer is : \n" + selected_df.tolist()
+        print(select_column)
+        json_df = json.loads(selected_df[select_column].to_json())
+        json_df_values = [str(list(i.values())[0]) for i in json_df.values()]
+        print(json_df_values)
+        response_msg = " : ".join(json_df_values)
+        print (response_msg)
     return send_to_ui(response_msg)
 
 def get_generic_response(input_text, k):
